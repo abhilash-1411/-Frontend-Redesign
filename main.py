aea29ca5c5d1672315551cad0f9cc55cadc9bd58
unknown <abhilash.chandravanshi@adixoo.com>
2024-10-14 11:53:11 +0530

    folder structure

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Welcome to the Flask API!"
+
+@app.route('/header')
+def header():
+    return render_template('header.html')
 
 
 

7d82e41a9b850c38fb34843502cab014281bd580
unknown <abhilash.chandravanshi@adixoo.com>
2024-10-24 11:24:12 +0530

    homepage

--- main.py
+++ main.py
@@ -44,6 +44,10 @@ def workspaceProducts():
 def dataSetup():
     return render_template('datasetup.html')
 
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html')
+
 
 
 if __name__ == '__main__':
